3bf5b06a6d7cd5485d7d7bcac8b6c00a32a8fcfb
ArinSunam <arinsunam05@gmail.com>
2025-01-28 04:17:41 +0545

    DB connection and server connection

---
+++ src/db/index.js
@@ -0,0 +1,12 @@
+import mongoose from "mongoose";
+
+
+export const dbConnect = async () => {
+  try {
+    const mongoInstance = await mongoose.connect(`${process.env.MONGODB_URI}`)
+    console.log(`Mongodb connected !! Host: ${mongoInstance.connection.host}`)
+  } catch (error) {
+    console.log("MONGODB CONNECTION ERROR:", error)
+    process.exit(1)
+  }
+}
---
+++ src/index.js
@@ -0,0 +1,22 @@
+import dotenv from 'dotenv'
+import express from 'express'
+const app = express()
+const port = 3000
+const hostname = '127.0.0.1'
+import { dbConnect } from './db/index.js'
+
+dotenv.config()
+
+
+dbConnect()
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+
+app.listen(port, () => {
+  console.log(`Running on http://${hostname}:${port}`)
+})
+
+
